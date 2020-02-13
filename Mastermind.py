d1336a2815f37111ce26c52a60775f86d3eeb2c8
kaiterhorst <kai.terhorst@student.hu.nl>
2020-02-13 11:21:12 +0100

    ini

--- Mastermind.py
+++ Mastermind.py
@@ -1,1 +1,13 @@
-hjgf
+''' interface gedeelte'''
+print('kies een optie.')
+optie = input('Optie 1: Kies zelf een patroon en de computer gokt'
+      '\nOptie 2: De computer kiest een patroon en jij gokt')
+
+''' loop optie 1'''
+while optie == 1:
+
+    code = input('kies een kleur code.')
+
+
+''' loop optie 2'''
+while optie == 2:
