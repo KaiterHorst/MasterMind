01538b6e8ea4b6d21450672efefc10ed1a7ea430
kaiterhorst <kai.terhorst@student.hu.nl>
2020-02-15 21:40:30 +0100

    ini

--- Mastermind.py
+++ Mastermind.py
@@ -1,13 +1,73 @@
+import random
 ''' interface gedeelte'''
-print('kies een optie.')
-optie = input('Optie 1: Kies zelf een patroon en de computer gokt'
-      '\nOptie 2: De computer kiest een patroon en jij gokt')
+print('Kies een optie.')
+print(  'Optie 1: De computer kiest een patroon en jij gokt'
+        '\nOptie 2: Kies zelf een patroon en de computer gokt, implementatie 1'
+        '\nOptie 3: Kies zelf een patroon en de computer gokt, implementatie 2'
+        '\nOptie 4: Kies zelf een patroon en de computer gokt, eige implementatie')
 
-''' loop optie 1'''
-while optie == 1:
+optie = input(('Maak een keuze:'))
 
-    code = input('kies een kleur code.')
+''' Optie 1'''
+if optie == '1':
+    print('\n''De kleuren waaruit je kan kiezen zijn: Rood, Blauw, Groen, Geel, Oranje en Paars\n')
+    kleuren = ['Rood', 'Blauw', 'Groen', 'Geel', 'Oranje', 'Paars']
+    PCcode = []
+    PCkleur1 = random.choice(kleuren)
+    PCkleur2 = random.choice(kleuren)
+    PCkleur3 = random.choice(kleuren)
+    PCkleur4 = random.choice(kleuren)
+    PCcode.append(PCkleur1)
+    PCcode.append(PCkleur2)
+    PCcode.append(PCkleur3)
+    PCcode.append(PCkleur4)
+    print(PCcode)
+    for i in range(1,11):
+        Gokken = []
+        Gok1 = input('Eerste kleur?')
+        Gokken.append(Gok1)
+        while Gok1 not in kleuren:
+            print('Dit is geen geldige keuze, probeer opnieuw.')
+            Gok1 = input('Eerste kleur?')
+
+        Gok2 = input('Tweede kleur?')
+        Gokken.append(Gok2)
+        while Gok2 not in kleuren:
+            print('Dit is geen geldige keuze, probeer opnieuw.')
+            Gok2 = input('Tweede kleur?')
+
+        Gok3 = input('Derde kleur?')
+        Gokken.append(Gok3)
+        while Gok3 not in kleuren:
+            print('Dit is geen geldige keuze, probeer opnieuw.')
+            Gok3 = input('Derde kleur?')
+
+        Gok4 = input('Vierde kleur?')
+        Gokken.append(Gok4)
+        while Gok4 not in kleuren:
+            print('Dit is geen geldige keuze, probeer opnieuw.')
+            Gok4 = input('Vierde kleur?')
+
+        Zwart = 0  # Goede kleur, Goede plek
+        Wit = 0  # Goede kleur, foute plek
+        print(Gokken)
+        for i in range(4):
+            if Gokken[i] == PCcode[i]:
+                Zwart += 1
+            if Gokken[i] in PCcode and Gokken[i] != PCcode[i]:
+                Wit += 1
 
 
-''' loop optie 2'''
-while optie == 2:
+
+
+
+
+
+
+        print('Je hebt', Zwart,'aantal pinnen op de juiste positie.'
+            '\nJe hebt', Wit, ' kleuren goed maar op de foute positie')
+
+        if Zwart == 4:
+            print('Je hebt binnen 10 zetten gewonnen!')
+            break
+    print('Helaas, je beurten zijn op.')
