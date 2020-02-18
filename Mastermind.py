1869dcae1d184ac29594c1ca94131aadbc09cbee
kaiterhorst <kai.terhorst@student.hu.nl>
2020-02-18 14:30:04 +0100

    Wit/Zwart gefixt

--- Mastermind.py
+++ Mastermind.py
@@ -1,13 +1,13 @@
 while True:
     ''' interface gedeelte'''
     print('Dit zijn de keuzes:'
-         '\n=======================================================================')
-    print(  'Optie 1: De computer kiest een patroon en jij gokt'
-            '\nOptie 2: Kies zelf een patroon en de computer gokt, implementatie 1'
-            '\nOptie 3: Kies zelf een patroon en de computer gokt, implementatie 2'
-            '\nOptie 4: Kies zelf een patroon en de computer gokt, eige implementatie'
-            '\n======================================================================='
-            '\nKies 1, 2, 3 of 4')
+          '\n=======================================================================')
+    print('Optie 1: De computer kiest een patroon en jij gokt'
+          '\nOptie 2: Kies zelf een patroon en de computer gokt, implementatie 1'
+          '\nOptie 3: Kies zelf een patroon en de computer gokt, implementatie 2'
+          '\nOptie 4: Kies zelf een patroon en de computer gokt, eige implementatie'
+          '\n======================================================================='
+          '\nKies 1, 2, 3 of 4')
 
     optie = input(('Maak een keuze:'))
 
@@ -15,6 +15,7 @@ while True:
     if optie == '1':
         import random
         from src import functies as func
+
         print('\n''De kleuren waaruit je kan kiezen zijn: Rood(R), Blauw(B), Groen(GR), Geel(G), Oranje(O) en Paars(P)'
               '\n')
         kleuren = ['R', 'B', 'Gr', 'G', 'O', 'P']
@@ -32,7 +33,7 @@ while True:
 
             print(PCcode)
             print('zwart:', func.pinchecker(Gokken, PCcode)[1],
-                '\nwit:', func.pinchecker(Gokken, PCcode)[0])
+                  '\nwit:', func.pinchecker(Gokken, PCcode)[0])
 
             if func.pinchecker(Gokken, PCcode)[1] == 4:
                 print('Je hebt binnen 10 zetten gewonnen!')
@@ -40,14 +41,7 @@ while True:
 
         print('Helaas, je beurten zijn op. De code was,', PCcode, '\nNog een keertje proberen?')
 
-
     '''Optie 2'''
     if optie == '2':
         import random
         from src import functies as func
-
-
-
-
-
-
--- functies.py
+++ functies.py
@@ -1,27 +1,29 @@
-def combinatielijst(elementenlijst, lengte):
+def combinatielijst(elementenlijst,lengte):
+# Deze functie genereert een lijst met alle combinaties in (elementenlijst) met de lengte (lengte).
     import itertools as it
     allecombinaties = []
-
 
     for j in it.product((elementenlijst), repeat=lengte):
         allecombinaties.append(j)
     return allecombinaties
 
 
-
-
-
 def pinchecker(Gokken, PCcode):
     Zwart = 0
     Wit = 0
-    Algebruikt = []
+    Tlijst = []
+
+
+    for i in range(4):
+        Tlijst.append(PCcode[i])
+
     for i in range(4):
         if Gokken[i] == PCcode[i]:
             Zwart += 1
-            Algebruikt.append(Gokken[i])
+            Tlijst.remove(Gokken[i])
 
     for j in range(4):
-        if Gokken[j] in PCcode and Gokken[j] != PCcode[j] and Gokken[j] not in Algebruikt:
+        if Gokken[j] in Tlijst and Gokken[j] != PCcode[j]:
             Wit += 1
 
     return Wit, Zwart
